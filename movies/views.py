d691921a7b03b1447c034ee22cf46ca8dc6bbb3c
diego-valdivia <64038852+diego-valdivia@users.noreply.github.com>
2020-12-12 11:15:17 -0500

    views para grafico

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from fusioncharts import FusionCharts
+from pprint import pprint
+
+
+def chart(request):
+    dataSource = OrderedDict()
+    chartConfig = OrderedDict()
+    chartConfig["caption"] = "Countries With Most Oil Reserves [2017-18]"
+    chartConfig["subCaption"] = "In MMbbl = One Million barrels"
+    chartConfig["xAxisName"] = "Country"
+    chartConfig["yAxisName"] = "Reserves (MMbbl)"
+    chartConfig["numberSuffix"] = "K"
+    chartConfig["theme"] = "fusion"
+
+    chartData = OrderedDict()
+    chartData["Venezuela"] = 290
+    chartData["Saudi"] = 260
+    chartData["Canada"] = 180
+    chartData["Iran"] = 140
+    chartData["Russia"] = 115
+    chartData["UAE"] = 100
+    chartData["US"] = 30
+    chartData["China"] = 30
+
+    dataSource["chart"] = chartConfig
+    dataSource["data"] = []
+    for key, value in chartData.items():
+        dataSource["data"].append({'label':key, 'value': value})
+
+    pprint(dataSource)
+    column2D = FusionCharts("column2d", "Oil_Reserves", "600", "400", "Oil_Reserves-container", "json", dataSource)
+    context = {'output': column2D.render(), }
+    return render(request, 'pag_no_encontrada.html', context)
+
 # Create your views here.
 def inicio(request):
     return render(request,'MovieDick_Inicio.html')
